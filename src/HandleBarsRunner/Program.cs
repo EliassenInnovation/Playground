57b288479adb4c44f35c1b775cb98a9bb2b83ab0
Matthew Whited <mwhited@eliassen.com>
2024-10-04 17:17:50 -0400

    add example

---
+++ src/HandleBarsRunner/Program.cs
@@ -0,0 +1,61 @@
+﻿using HandlebarsDotNet;
+using HandlebarsDotNet.Extension.Json;
+using Microsoft.Data.SqlClient;
+using System.Text;
+using System.Text.Json;
+
+namespace HandleBarsRunner
+{
+    internal class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var sqlResource = "HandleBarsRunner.EnumerateDatabase.sql";
+            using var sqlScriptStream = typeof(Program).Assembly.GetManifestResourceStream(sqlResource);
+            using var sqlScriptReader = new StreamReader(sqlScriptStream);
+            var sqlScript = sqlScriptReader.ReadToEnd();
+
+            var connectionString = "Server=127.0.0.1;Database=GreenOnionDb;User ID=sa;Password=Gr33n0n!on;TrustServerCertificate=True;";
+            using var sqlConn = new SqlConnection(connectionString);
+            await sqlConn.OpenAsync();
+            using var sqlCmd = new SqlCommand(sqlScript, sqlConn);
+            using var sqlreader = sqlCmd.ExecuteReader();
+
+            var sqlJson = new StringBuilder();
+            while (sqlreader.Read())
+            {
+                using var textReader = sqlreader.GetTextReader(0);
+                
+                var text = textReader.ReadToEnd();
+                sqlJson.Append(text);
+            }
+
+            var json = sqlJson.ToString();
+
+            //var fileName = "dbSchema.json";
+            var templateFile = "dbSchema.jdl.hbs";
+            var outFile = "dbSchema.jdl";
+
+            //using var jsonFile = File.OpenRead(fileName);
+            var data = JsonDocument.Parse(json);
+
+            // https://github.com/Handlebars-Net/Handlebars.Net
+
+            var config = new HandlebarsConfiguration()
+            {
+                NoEscape = true,
+            };
+            var handlebar = Handlebars.Create(config);
+            handlebar.Configuration.UseJson();
+
+            var reader = new StreamReader(templateFile);
+            var compiled = handlebar.Compile(reader);
+
+            using var writer = File.CreateText(outFile);
+            compiled(writer, context: data);
+            await writer.FlushAsync();
+
+
+        }
+    }
+}
---
+++ src/JiraSplitter/Program.cs
@@ -0,0 +1,77 @@
+﻿using System.Runtime.CompilerServices;
+using System.Text;
+using System.Xml.Linq;
+
+namespace JiraSplitter
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var file = @"Nucleus-JIRA.xml";
+            var output = @"C:\Repos\github\ExampleDocs\JIRA\Nucleus Demo Modules";
+
+            var xml = XDocument.Load(file);
+            var converter = new ReverseMarkdown.Converter();
+
+            if (!Directory.Exists(output))
+            {
+                Directory.CreateDirectory(output);
+            }
+
+            var items = xml.Element("rss").Element("channel").Elements("item");
+
+            foreach (var item in items)
+            {
+                var title = (string)item.Element("title");
+                var link = (string)item.Element("link");
+                var project = (string)item.Element("project");
+                var projectKey = (string)item.Element("project").Attribute("key");
+                var key = (string)item.Element("key");
+                var summary = (string)item.Element("summary");
+
+                var type = (string)item.Element("type");
+                var priority = (string)item.Element("priority");
+                var status = (string)item.Element("status");
+                var resolution = (string)item.Element("resolution");
+                var assignee = (string)item.Element("assignee");
+                var reporter = (string)item.Element("reporter");
+
+                var html = (string)item.Element("description");
+                var markdown = converter.Convert(html);
+
+                var comments = string.Join(Environment.NewLine, item.Element("comments")?.Elements("comment")?.Select(x => $" - {x.Attribute("created")}: {converter.Convert((string)x)}")?? Enumerable.Empty<string>());
+
+                var result = @$"# {title}
+
+## Summary
+
+{key} [{summary}]({link}) is related to {project}
+
+- Type: {type}
+- Priority: {priority}
+- Status: {status}
+- Resolution: {resolution}
+- Assignee: {assignee}
+- Reporter: {reporter}
+
+## Description
+
+{markdown}
+
+## Comments
+
+{comments}
+
+";
+                var clientTitle = new[] { '\\', '/', '&', ':', '\r', '\n', '\"', '\'' }.Aggregate(new StringBuilder(title), (sb, i) => sb.Replace(i, '-')).ToString();
+
+                var fileName = Path.Combine(output, $"{clientTitle}.md");
+                Console.WriteLine(title);
+                File.WriteAllText(fileName, result);
+            }
+
+            Console.WriteLine("fin!");
+        }
+    }
+}
